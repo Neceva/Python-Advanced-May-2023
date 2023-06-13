5b2d25a967644f665823f87154d3310bf0be020f
Anna-Mariya Netseva-Borikova <a.neceva@gmail.com>
2023-06-13 21:36:57 +0300

    update

--- exam_prep/01_apocalypse_preparation.py
+++ exam_prep/01_apocalypse_preparation.py
@@ -1,29 +1,34 @@
 from collections import deque
 
+HEALING_ITEM_THRESHOLD = 100
+PATCH_THRESHOLD = 30
+BANDAGE_THRESHOLD = 40
+
 healingItems = {"Patch": 0, "Bandage": 0, "MedKit": 0}
 
 textiles = deque(int(x) for x in input().split())
 medicaments = deque(int(x) for x in input().split())
 
 while textiles and medicaments:
-    textile = textiles.popleft()
-    medicament = medicaments.pop()
+    current_textile = textiles.popleft()
+    current_medicament = medicaments.pop()
 
-    summed = textile + medicament
+    summed = current_textile + current_medicament
 
-    if summed == 30:
+    if summed == PATCH_THRESHOLD:
         healingItems["Patch"] += 1
-    elif summed == 40:
+    elif summed == BANDAGE_THRESHOLD:
         healingItems["Bandage"] += 1
-    elif summed == 100:
+    elif summed == HEALING_ITEM_THRESHOLD:
         healingItems["MedKit"] += 1
-    elif summed > 100:
+    elif summed > HEALING_ITEM_THRESHOLD:
         healingItems["MedKit"] += 1
-        remaining_resources = summed - 100
-        medicaments[-1] += remaining_resources
+        summed -= 100
+        medicaments[-1] += summed
     else:
-        medicament += 10
-        medicaments.append(medicament)
+        current_medicament += 10
+        medicaments.append(current_medicament)
+
 if textiles:
     print("Medicaments are empty.")
 elif medicaments:
@@ -31,6 +36,15 @@ elif medicaments:
 else:
     print("Textiles and medicaments are both empty.")
 
+sort_result = sorted(healingItems.items(), key=lambda x: (-x[1], x[0]))
 
+for item in sort_result:
+    if int(item[1]) > 0:
+        print(f"{item[0]} - {item[1]}")
 
+if textiles:
+    print(f"Textiles left: {(', '.join(str(el) for el in textiles))}")
+if medicaments:
+    medicaments.reverse()
+    print(f"Medicaments left: {(', '.join(str(el) for el in medicaments))}")
 
